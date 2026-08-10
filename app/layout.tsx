41c1f655a4680371f8cd1e725de84b2d49d68c54
Hindustan Projects <hindustanprojects@gmail.com>
2026-08-10 18:52:30 +0530

    Update construction site SEO and settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -27,15 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${montserrat.variable}`}>
       <body className="font-sans">
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-0RSKYLDLLY" strategy="afterInteractive" />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0RSKYLDLLY');
-          `}
-        </Script>
         {children}
       </body>
     </html>
